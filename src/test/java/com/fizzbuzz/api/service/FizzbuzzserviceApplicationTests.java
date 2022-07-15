70e208034f137d6f75effa7c624c883424c22f19
Parnika <govind24n@gmail.com>
2022-07-15 15:09:19 +0530

    test case written to checking of result for given number expected fizz and it failed.

--- src/test/java/com/fizzbuzz/api/service/FizzbuzzserviceApplicationTests.java
+++ src/test/java/com/fizzbuzz/api/service/FizzbuzzserviceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fizzbuzz.api.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -7,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest
 class FizzbuzzserviceApplicationTests {
@@ -19,5 +23,16 @@ class FizzbuzzserviceApplicationTests {
 		  mockMvc.perform(get("/fizzbuzzplay/2")) 
 		  .andExpect(status().is(200));
     }
+	
+	@Test
+	void getFizzToGivenNumber() throws Exception {
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/fizzbuzzplay/3");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals("Fizz", result.getResponse().getContentAsString());
+		
+		System.out.println(result.getResponse().getContentAsString());
+	}
 
 }
