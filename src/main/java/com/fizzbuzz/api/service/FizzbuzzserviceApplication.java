d1db4d79951ea1d33cfc89eac8c0d6c8fcca7909
Parnika <govind24n@gmail.com>
2022-07-15 12:39:35 +0530

    Initial commit with skelton to make fizzbuzzservice

---
+++ src/main/java/com/fizzbuzz/api/service/FizzbuzzserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fizzbuzz.api.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fizzbuzz/api/service/FizzbuzzserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizzbuzz.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
